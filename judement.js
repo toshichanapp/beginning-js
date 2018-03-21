923755c6e2eb41cb9545b1a38a267c65c474d085
fuzisan <toshichanapp@gmail.com>
2018-03-21 08:09:09 +0000

    task1

---
+++ judement.js
@@ -0,0 +1,20 @@
+function get_achievement(x, y, z) {
+
+    let sum = x + y + z;
+
+    if (sum >= 250) {
+        return "A";
+    }
+    else if (sum >= 200) {
+        return "B";
+    }
+    else if (sum >= 100) {
+        return "C";
+    }
+    else {
+        return "D";
+    }
+
+};
+
+console.log(get_achievement(60, 100, 40));
