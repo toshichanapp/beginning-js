3ebde58785ff3110c240802f0208e1a66041963a
fuzisan <toshichanapp@gmail.com>
2018-03-21 08:50:04 +0000

    task

--- judement.js
+++ judement.js
@@ -1,14 +1,15 @@
-function get_achievement(x, y, z) {
+function get_achievement(points) {
 
-    let sum = x + y + z;
+    let perfectScore = 100 * points.length;
+    let sum = points.reduce(function(sum, next) { return sum + next }, 0);
 
-    if (sum >= 250) {
+    if (sum >= perfectScore * 0.8) {
         return "A";
     }
-    else if (sum >= 200) {
+    else if (sum >= perfectScore * 0.6) {
         return "B";
     }
-    else if (sum >= 100) {
+    else if (sum >= perfectScore * 0.4) {
         return "C";
     }
     else {
@@ -17,4 +18,25 @@ function get_achievement(x, y, z) {
 
 };
 
-console.log(get_achievement(60, 100, 40));
+function get_pass_or_failure(points) {
+    let judge = "合格";
+    for (let i = 0; i < points.length; i++) {
+        if (points[i] < 60) {
+            judge = "不合格";
+            break;
+        }
+    }
+
+    return judge;
+};
+
+function judgement(points) {
+    let achievement = get_achievement(points);
+    let pass_or_failure = get_pass_or_failure(points);
+
+    return `あなたの成績は${achievement}です。${pass_or_failure}です！`;
+};
+
+
+let points = [50, 100, 80, 80, 70, 80];
+console.log(judgement(points));
